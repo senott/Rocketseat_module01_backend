bcaaafb03fede8b1cb9ee7620bd08b1baad509d9
Senott <senott@yahoo.com>
2020-08-18 11:11:19 -0300

    Request params.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query
+
+  console.log(title)
+  console.log(owner)
+
   return response.json([
     'Projeto 1',
     'Projeto 2'
@@ -10,6 +17,11 @@ app.get('/projects', (request, response) => {
 })
 
 app.post('/projects', (request, response) => {
+  const { title, owner } = request.body
+
+  console.log(title)
+  console.log(owner)
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -18,6 +30,10 @@ app.post('/projects', (request, response) => {
 })
 
 app.put('/projects/:id', (request, response) => {
+  const { id } = request.params
+
+  console.log(id)
+
   return response.json([
     'Projeto 4',
     'Projeto 2',
